c36e28080e7812b328b2fc19bd4a93bf94080ac8
Stephen Collis <stephen.collis@lds-research.co.uk>
2026-04-18 13:44:15 +0100

    deploy test

--- wp-content/themes/vinttro_child_theme/listings/content-single-listing.php
+++ wp-content/themes/vinttro_child_theme/listings/content-single-listing.php
@@ -25,7 +25,7 @@ if ( post_password_required() ) {
 <div id="listing-<?php the_ID(); ?>" class="auto-listings-single listing">
 
         <div class="full-width upper">
-        <p>Hello, World! 2!</p>
+        <p>Hello, World! 33!</p>
                 <?php
                 /**
                  * @hooked auto_listings_template_single_title

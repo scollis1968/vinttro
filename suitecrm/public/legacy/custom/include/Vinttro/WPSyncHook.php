9ddf6b80e791cff2eb546ce7227cf49dbc522201
Stephen Collis <stephen.collis@lds-research.co.uk>
2026-05-27 19:40:07 +0100

    fleet pub 6

--- suitecrm/public/legacy/custom/include/Vinttro/WPSyncHook.php
+++ suitecrm/public/legacy/custom/include/Vinttro/WPSyncHook.php
@@ -167,7 +167,7 @@ class WPSyncHook {
         file_put_contents($log, " - [STEP 3] Distributing updates for Fleet: " . $fleetBean->name . "\n", FILE_APPEND);
 
         // Name of relationship between Fleets and your new Membership module
-        $rel_fleet_memberships = 'visp_fleet_visp_fleet_membership';
+        $rel_fleet_memberships = 'visp_fleet_visp_fleet_memberships';
         file_put_contents($log, "   - Loading Fleet -> Membership relationship: '$rel_fleet_memberships'\n", FILE_APPEND);
 
         if ($fleetBean->load_relationship($rel_fleet_memberships)) {

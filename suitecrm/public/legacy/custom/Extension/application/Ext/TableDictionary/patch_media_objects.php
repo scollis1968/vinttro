49d81d02525924c06ebcfba7992791949387adc4
Stephen Collis <stephen.collis@lds-research.co.uk>
2026-06-04 10:52:23 +0100

    QRR log error

---
+++ suitecrm/public/legacy/custom/Extension/application/Ext/TableDictionary/patch_media_objects.php
@@ -0,0 +1,7 @@
+<?php
+// Core patch to satisfy legacy relationship engine and keep log files clean
+$dictionary['media_objects'] = array(
+    'table' => 'media_objects',
+    'fields' => array(),
+    'relationships' => array(),
+);

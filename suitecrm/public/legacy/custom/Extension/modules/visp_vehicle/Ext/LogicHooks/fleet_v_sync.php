1cad83487ca2b15a540e9d122f14d034400c07da
Stephen Collis <stephen.collis@lds-research.co.uk>
2026-06-18 19:08:46 +0100

    repush hooks

--- suitecrm/public/legacy/custom/Extension/modules/visp_vehicle/Ext/LogicHooks/fleet_v_sync.php
+++ suitecrm/public/legacy/custom/Extension/modules/visp_vehicle/Ext/LogicHooks/fleet_v_sync.php
@@ -1,6 +1,6 @@
 <?php
 $hook_array['after_save'][] = array(
-    10, // Fires after native saving operations
+    10, // Fires after native saving operations.
     'Sync Fleet Admins on Vehicle Change',
     'custom/include/Vinttro/WPSyncHook.php',
     'WPSyncHook',

4daa1b3aa13b4899f8a17f730acfacb8d112ce98
Stephen Collis <stephen.collis@lds-research.co.uk>
2026-06-20 15:05:51 +0100

    fleet-admin 3

--- wp-content/plugins/vinttro2.0/includes/membership-functions.php
+++ wp-content/plugins/vinttro2.0/includes/membership-functions.php
@@ -78,18 +78,20 @@ add_shortcode('fleet_dashboard', function() {
     <?php
     return ob_get_clean();
 });
-
 add_shortcode('fleet_admin', function() {
-    if (!is_user_logged_in()) {
+    if ( ! is_user_logged_in() ) {
         return '<p>Please <a href="/login">log in</a> to view your dashboard.</p>';
     }
 
     $user_id = get_current_user_id();
-    $user_info = get_userdata($user_id);
 
     ob_start(); 
     ?>
-            <?php echo vinttro_get_fleet_panels($user_id); ?>           
+    <div class="vinttro-dashboard vinttro-admin-dashboard-view">
+        <div class="dashboard-grid">
+            <?php echo vinttro_get_fleet_panels( $user_id ); ?>          
+        </div>
+    </div>
     <?php
     return ob_get_clean();
 });

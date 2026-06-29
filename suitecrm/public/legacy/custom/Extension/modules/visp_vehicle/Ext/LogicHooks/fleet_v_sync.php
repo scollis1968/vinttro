1dd9688c7ce4175f2904f371e84ba13c876fa118
Stephen Collis <stephen.collis@lds-research.co.uk>
2026-06-29 07:34:40 +0100

    vc 2

--- suitecrm/public/legacy/custom/Extension/modules/visp_vehicle/Ext/LogicHooks/fleet_v_sync.php
+++ suitecrm/public/legacy/custom/Extension/modules/visp_vehicle/Ext/LogicHooks/fleet_v_sync.php
@@ -3,6 +3,6 @@ $hook_array['after_save'][] = array(
     10, // Fires after native saving operations.
     'Sync Fleet Admins on Vehicle Change',
     'custom/include/Vinttro/WPSyncHook.php',
-    'WPSyncHook',
+    'VinttroWPSyncMaster',
     'syncVehicleFleetUpdate'
 );

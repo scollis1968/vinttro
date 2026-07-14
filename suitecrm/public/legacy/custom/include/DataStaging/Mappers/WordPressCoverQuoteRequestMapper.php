7f163c240625ae32fa9d0cad3541f74b4ac2b8c2
Stephen Collis <stephen.collis@lds-research.co.uk>
2026-07-14 17:05:55 +0100

    +1

--- suitecrm/public/legacy/custom/include/DataStaging/Mappers/WordPressCoverQuoteRequestMapper.php
+++ suitecrm/public/legacy/custom/include/DataStaging/Mappers/WordPressCoverQuoteRequestMapper.php
@@ -4,13 +4,13 @@ namespace Custom\DataStaging\Mappers;
 use Custom\DataStaging\AbstractStagingMapper;
 use BeanFactory;
 
-class WordPressCoverQuoteRequestMapper  extends AbstractStagingMapper {
+class WordPressCoverQuoteRequestMapper extends AbstractStagingMapper {
     
+    // Removed 'email' => 'email1' from here to process it safely via the API below
     private array $fieldMap = [
-        'first-name'    => 'first_name',
-        'last-name'     => 'last_name',
-        'email' => 'email1',
-        'postcode' => 'primary_address_postalcode'
+        'first-name' => 'first_name',
+        'last-name'  => 'last_name',
+        'postcode'   => 'primary_address_postalcode'
     ];
 
     public function process(array $rawData, \SugarBean $stagingRecord) {
@@ -23,15 +23,20 @@ class WordPressCoverQuoteRequestMapper  extends AbstractStagingMapper {
             throw new \Exception("CRM requires a 'last-name' to save a contact card.");
         }
         
-        // 2. Contact Lookup via your parent abstract class
+        // 2. Contact Lookup via safely retrieving the bean
         $contactId = $this->findContactIdByEmail($rawData['email']);
+        $contact = null;
 
         if ($contactId) {
-            $contact = BeanFactory::getBean('Contacts', $contactId);
-            $action = "Updated existing Contact";
-        } else {
+            // retrieveBean returns null safely if the record doesn't exist or is deleted
+            $contact = BeanFactory::retrieveBean('Contacts', $contactId);
+        }
+
+        if (empty($contact) || empty($contact->id)) {
             $contact = BeanFactory::newBean('Contacts');
             $action = "Created new Contact";
+        } else {
+            $action = "Updated existing Contact";
         }
     
         // 3. Custom Phone Routing Logic
@@ -50,16 +55,21 @@ class WordPressCoverQuoteRequestMapper  extends AbstractStagingMapper {
             if (array_key_exists($sourceField, $rawData)) {
                 $value = $rawData[$sourceField];
 
-                if (property_exists($contact, $crmField) || isset($contact->field_defs[$crmField])) {
+                // Rely strictly on field_defs since CRM properties are dynamic
+                if (isset($contact->field_defs[$crmField])) {
                     $contact->$crmField = $value;
                 }
             }
         }
+
+        // 5. Explicitly handle email addressing safely for both updates and inserts
+        if (!empty($rawData['email']) && isset($contact->emailAddress)) {
+            $contact->emailAddress->addAddress($rawData['email'], true); // 'true' marks it as primary
+        }
     
-        // 5. Save the Contact
+        // 6. Save the Contact
         $contact->save();
 
-        // This string feeds directly back to the scheduler to populate your feedback field
         return "{$action} successfully. Target CRM ID: {$contact->id}";
     }
 }
